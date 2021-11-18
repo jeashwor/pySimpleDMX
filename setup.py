ff081ae7b81ab6aec512696916e1de631c8cbd34
Jacob Ashworth <jeashwor@gmail.com>
2021-11-18 15:05:32 -0500

    updated setup.py to add python 3.7 to list of Programming languages

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Programming Language :: Python",
         'Programming Language :: Python :: 2',
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
         "Intended Audience :: Other Audience",
         "Topic :: Artistic Software",
     ]
